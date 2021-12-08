fabedc3a0c048ef31019695c3e9ed260cd31a8cd
Henry Dewsnap <74488894+HenryDewsnap@users.noreply.github.com>
2021-12-08 22:28:06 +0000

    Added new endpoint
    the endpoint /SCheck/  AKA "super check" allows for the program to check the for even more conditions of banned words.

--- API.py
+++ API.py
@@ -13,13 +13,36 @@ api = Api(app)
 
 #Censor Settings:
 disallowed   = ["frick"]
+disallowedOrder = []
 replacements = {"!":"i"}
+
+def compileDisallowedOrder():
+    for word in disallowed:
+        disallowedOrder.append(convertWordToCharacters(word))
+        
+
+def convertWordToCharacters(wordArg):
+    word = ""
+    previousLetter = ""
+    for letter in list(wordArg.lower()):
+        if letter != previousLetter:
+            word += letter
+        previousLetter = letter
+    return word
 
 #Alone this is not very thorough and is easily bypassed.
 def isWordAllowed(word):
     if word.lower() in disallowed: 
         return False
     return True
+
+
+def checkLetterOrder(word):
+    wordLetterOrder = convertWordToCharacters(word)
+    if wordLetterOrder in disallowedOrder:
+        return False
+    return True
+
 
 def replaceAlternatives(word):
     output = ""
@@ -30,10 +53,14 @@ def replaceAlternatives(word):
         output += letter
     return output
 
+
+
+
+
 ##Much faster but more vulnerable to being evaded.
 class quickCheck(Resource):
     def get(self, message):
-        wprt = -1 #Word Ptr
+        wprt = 0 #Word Ptr
         output = ""
         censored = False
         for word in message.split(diameter):
@@ -52,7 +79,7 @@ class quickCheck(Resource):
 ##Much slower but less vulnerable to being evaded.
 class thoroughCheck(Resource):
     def get(self, message):
-        wprt = -1 #Word Ptr
+        wprt = 0 #Word Ptr
         output = ""
         censored = False
         for word in message.split(diameter):
@@ -68,8 +95,33 @@ class thoroughCheck(Resource):
             if censored == True: return "Censored"
             return output
 
+##The slowest but most thorough.
+class superCheck(Resource):
+    def get(self, message):
+        wprt = 0 #Word Ptr
+        output = ""
+        censored = False
+        for word in message.split(diameter):
+            wprt += 1
+            if isWordAllowed(replaceAlternatives((word))) == False or checkLetterOrder(word) == False: 
+                censored = True
+                output += f"{censorChar}" * len(word)
+            else: output += word
+            if wprt < len(message.split(diameter)): output += " "
+        if returnCensored == True: return output
+        else: 
+            if censored == True: return "Censored"
+            return output
+
+
+
+
 api.add_resource(quickCheck, "/QCheck/<message>")    #End Point -1
 api.add_resource(thoroughCheck, "/TCheck/<message>") #End Point -2
+api.add_resource(superCheck, "/SCheck/<message>") #End Point -3
 
 if __name__ == "__main__":
+    compileDisallowedOrder()
+    print(f"Dissalowed Words: {disallowed}")
+    print(f"Dissalowed Orders: {disallowedOrder}")
     app.run(port=PORT)
